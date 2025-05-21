3aa283cdf12bc042aa1c3d8ba9f5e079a7719ad8
SwethajaG <83750903+SwethajaG@users.noreply.github.com>
2025-05-21 22:57:09 +0530

    Add files via upload
    Interactive Boxes - Commit 1

---
+++ script.js
@@ -0,0 +1,23 @@
+const expansionPanels = document.querySelectorAll('.panel');
+
+expansionPanels.forEach(panel => {
+
+  const header = panel.querySelector('.panel-header');
+  const content = panel.querySelector('.panel-content');
+
+
+	panel.addEventListener('mouseenter', () => {
+    header.classList.add('active');
+    content.classList.add('active');
+	
+	panel.style.borderColor = "#e67387";
+	panel.style.borderWidth  = "2px";
+ 
+  });
+  panel.addEventListener('mouseleave', () => {
+    header.classList.remove('active');
+    content.classList.remove('active');
+	panel.style.borderColor = "#b3a7a7";
+	panel.style.borderWidth  = "1px";	
+  });
+});
