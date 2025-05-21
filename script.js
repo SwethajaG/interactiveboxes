cb7a07ab8d0e14e48fc44fc541569ce879899018
SwethajaG <83750903+SwethajaG@users.noreply.github.com>
2025-05-21 23:36:51 +0530

    Update script.js
    Prettier Update

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 const expansionPanels = document.querySelectorAll('.panel');
 
 expansionPanels.forEach(panel => {
-
-  const header = panel.querySelector('.panel-header');
-  const content = panel.querySelector('.panel-content');
-
-
+  	const header = panel.querySelector('.panel-header');
+  	const content = panel.querySelector('.panel-content');
 	panel.addEventListener('mouseenter', () => {
-    header.classList.add('active');
-    content.classList.add('active');
-	
-	panel.style.borderColor = "#e67387";
-	panel.style.borderWidth  = "2px";
- 
-  });
-  panel.addEventListener('mouseleave', () => {
-    header.classList.remove('active');
-    content.classList.remove('active');
-	panel.style.borderColor = "#b3a7a7";
-	panel.style.borderWidth  = "1px";	
-  });
+    		header.classList.add('active');
+    		content.classList.add('active');
+		panel.style.borderColor = "#e67387";
+		panel.style.borderWidth  = "2px";
+ 	});
+  	panel.addEventListener('mouseleave', () => {
+    		header.classList.remove('active');
+    		content.classList.remove('active');
+		panel.style.borderColor = "#b3a7a7";
+		panel.style.borderWidth  = "1px";	
+  	});
 });
